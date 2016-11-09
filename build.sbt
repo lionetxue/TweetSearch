60f582da57cba2137c84e5943839d29256df3894
Lin Xue <lx58@cornell.edu>
2016-11-09 09:51:14 -0500

    improve search function to filter tweets contains geo info and add rate limit count

--- build.sbt
+++ build.sbt
@@ -2,10 +2,10 @@ name := "TwitterSearch"
 
 version := "0.1"
 
-scalaVersion := "2.11.7"
+scalaVersion := "2.11.8"
 
 resolvers += Resolver.sonatypeRepo("releases")
 
 libraryDependencies ++= Seq(
-  "com.danielasfregola" %% "twitter4s" % "2.0"
+  "com.danielasfregola" %% "twitter4s" % "2.2"
 )
--- src/main/scala/rest/SearchAndSaveTweets.scala
+++ src/main/scala/rest/SearchAndSaveTweets.scala
@@ -19,19 +19,21 @@ object SearchAndSaveTweets extends App with FileSupport {
 
   // TODO - Make sure to define your consumer and access tokens!
   val client = TwitterRestClient()
-
+  var apiCount = 0
   def searchTweets(query: String, max_id: Option[Long] = None): Future[Seq[Tweet]] = {
     def extractNextMaxId(params: Option[String]): Option[Long] = {
       //example: "?max_id=658200158442790911&q=%23scala&include_entities=1&result_type=mixed"
       params.getOrElse("").split("&").find(_.contains("max_id")).map(_.split("=")(1).toLong)
     }
+    apiCount += 1
     //val radius = Accuracy(10, Measure.Meter) 
-    //client.searchTweet(query, count = 100, result_type = ResultType.Recent, geocode = Some(GeoCode(42.964793, -75.48706, radius)), language = Some(Language.English), until= Some(ld), max_id = max_id).flatMap { result =>
-    //val ld = LocalDate.of(2016,10,22)
+    //client.searchTweet(query, count = 100, result_type = ResultType.Recent, geocode = Some(GeoCode(42.964793, -75.48706, radius)), language = Some(Language.English), until= Some(ld), since_id = Some(789069905593237505L), max_id = max_id).flatMap { result =>
+    //val ld = LocalDate.of(2016,11,8)
     client.searchTweet(query, count = 100, result_type = ResultType.Recent, max_id = max_id).flatMap { result =>
         val nextMaxId = extractNextMaxId(result.search_metadata.next_results)
         val tweets = result.statuses
-        if (tweets.nonEmpty) searchTweets(query, nextMaxId).map(_ ++ tweets)
+//        Future(tweets.sortBy(_.created_at))
+        if (tweets.nonEmpty && apiCount<15) searchTweets(query, nextMaxId).map(_ ++ tweets)
         else Future(tweets.sortBy(_.created_at))
       } recover { case _ => Seq.empty }
   }
@@ -41,9 +43,24 @@ object SearchAndSaveTweets extends App with FileSupport {
     config.getString("tweets.scalax")
   }
 
-  searchTweets("#cuzincks").map { tweets =>
+  var filteredtweets : Seq[Tweet] = Seq.empty
+  searchTweets("#CUZincks").map { tweets =>
     println(s"Downloaded ${tweets.size} tweets")
-    toFileAsJson(filename, tweets)
+    for (tweet <- tweets) {
+      tweet.coordinates match {
+        case Some(c) =>
+           println("Some coordinates")
+           filteredtweets :+= tweet
+        case _ => tweet.place match {
+          case Some(p) =>
+            println("Some place")
+            filteredtweets :+= tweet
+          case _ =>  None
+        }
+      }
+    }
+    println(s"Downloaded ${filteredtweets.size} tweets")
+    toFileAsJson(filename, filteredtweets)
     println(s"Tweets saved to file $filename")
   }
 
